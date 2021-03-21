2a83884f32707184cd05ac9edd2436c066bef868
unknown <vuonghk1@gmail.com>
2021-03-21 11:04:53 +0700

    Add rating model

---
+++ app/Models/Rating.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+    protected $table = 'rating';
+    protected $fillable = ['product_id','number','content','user_id'];
+}
