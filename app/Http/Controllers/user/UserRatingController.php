1f6f3ab70e9ef7faf57f7ad22ec783105becc1ac
unknown <vuonghk1@gmail.com>
2021-03-22 10:25:35 +0700

    Add RatingController

---
+++ app/Http/Controllers/user/UserRatingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserRatingController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+    public function saveRating(Request $request, $id)
+    {
+
+    }
+}
