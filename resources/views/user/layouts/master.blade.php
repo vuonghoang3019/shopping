8a4f4875b1a7d0f1ba6b51d103891fd26aef97b6
unknown <vuonghk1@gmail.com>
2021-03-22 10:51:32 +0700

    Add meta token

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     @yield('title')
     <link href="{{ asset('asset/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('asset/css/font-awesome.min.css') }}" rel="stylesheet">
