07296d9524401413ef47f3312eb68c54f4900b22
unknown <vuonghk1@gmail.com>
2021-03-31 21:40:08 +0700

    Add nothing

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     @yield('title')
     <link href="{{ asset('asset/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('asset/css/font-awesome.min.css') }}" rel="stylesheet">
@@ -15,16 +15,16 @@
     @yield('css')
 </head>
 <body>
-    @include('user.components.header')
-    @yield('content')
-    @include('user.components.footer')
+@include('user.components.header')
+@yield('content')
+@include('user.components.footer')
 </body>
-    <script src="{{ asset('asset/js/jquery.js') }}"></script>
-    <script src="{{ asset('asset/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('asset/js/jquery.scrollUp.min.js') }}"></script>
-    <script src="{{ asset('asset/js/price-range.js') }}"></script>
-    <script src="{{ asset('asset/js/jquery.prettyPhoto.js') }}"></script>
-    <script src="{{ asset('asset/js/main.js') }}"></script>
-    <script src="{{ asset('asset/js/html5shiv.js') }}"></script>
-    @yield('js')
+<script src="{{ asset('asset/js/jquery.js') }}"></script>
+<script src="{{ asset('asset/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('asset/js/jquery.scrollUp.min.js') }}"></script>
+<script src="{{ asset('asset/js/price-range.js') }}"></script>
+<script src="{{ asset('asset/js/jquery.prettyPhoto.js') }}"></script>
+<script src="{{ asset('asset/js/main.js') }}"></script>
+<script src="{{ asset('asset/js/html5shiv.js') }}"></script>
+@yield('js')
 </html>
